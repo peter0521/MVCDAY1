0b89f94ec8301d2023a2cb7ccd72afb68b6b6849
peter <peter0521@gmail.com>
2015-07-11 00:57:59 +0800

    https://github.com/peter0521/MVCDAY1.git

---
+++ MVC5DAY1/Controllers/Customer_ViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5DAY1.Models;
+
+namespace MVC5DAY1.Controllers
+{
+    public class Customer_ViewController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: Customer_View
+        public ActionResult Index()
+        {
+            return View(db.Customer_View.ToList());
+        }
+    }
+}
---
+++ MVC5DAY1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5DAY1.MvcApplication" Language="C#" %>
