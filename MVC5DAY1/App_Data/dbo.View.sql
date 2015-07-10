87e80df2422a5e3af27ac8f439c049a05fc2e8f8
peter <peter0521@gmail.com>
2015-07-11 02:06:13 +0800

    https://github.com/peter0521/MVCDAY1.git

---
+++ MVC5DAY1/App_Data/dbo.View.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW Customer_View
+             AS
+             SELECT 客戶名稱 ,
+					聯絡人數量 =(SELECT COUNT(客戶id)
+					FROM 客戶聯絡人
+					WHERE  客戶聯絡人.客戶id = 客戶資料.Id),
+
+					銀行帳戶數量 =(SELECT COUNT(客戶id)
+					FROM 客戶銀行資訊
+					WHERE  客戶銀行資訊.客戶id = 客戶資料.Id)
+
+             FROM 客戶資料
+
+/* 有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，用一個表格呈現報表即可。 */
